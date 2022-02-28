b91a81c8b7dd55f893727a32071ea5124074ea4e
Roman Zavelev <rzav@mail.ru>
2022-02-28 09:56:08 +0300

    1

--- src/main/java/com/expert1c/taskmgr/GreetingController.java
+++ src/main/java/com/expert1c/taskmgr/GreetingController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,10 +16,12 @@ public class GreetingController {
     @Autowired
     private TaskRepository taskRepository;
 
-    @GetMapping("/greeting")
+    @GetMapping("/listTasks")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        Iterable<Task> tasks = taskRepository.findAll();
         model.addAttribute("name", name);
-        return "greeting";
+        model.addAttribute("tasks", tasks);
+        return "listTasks";
     }
 
     @GetMapping("/")
@@ -28,6 +31,16 @@ public class GreetingController {
         model.addAttribute("name", name);
         model.addAttribute("tasks", tasks);
         return "main";
+    }
+
+     @GetMapping("listTasks/task-add")
+    public String taskAdd(Model model) {
+        return "task-add";
+    }
+
+    @PostMapping("listTasks/task-add")
+    public String taskPostAdd(@RequestParam String content, @RequestParam String author, Model model) {
+        return "";
     }
 
    /* @GetMapping("/main")
