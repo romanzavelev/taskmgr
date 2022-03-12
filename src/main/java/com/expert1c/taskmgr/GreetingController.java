3ecdbf5dce6e27a3ccd8ff6fa1ae2b0b05127a1c
Roman Zavelev <rzav@mail.ru>
2022-03-12 22:42:51 +0300

    1

--- src/main/java/com/expert1c/taskmgr/GreetingController.java
+++ src/main/java/com/expert1c/taskmgr/GreetingController.java
@@ -4,12 +4,14 @@ package com.expert1c.taskmgr;
 import com.expert1c.taskmgr.model.Task;
 import com.expert1c.taskmgr.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
@@ -27,9 +29,9 @@ public class GreetingController {
     }
 
     @GetMapping("/listTasks")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        Iterable<Task> tasks = taskRepository.findAll();
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(defaultValue = "0") int page, Model model) {
+        PageRequest pageable = PageRequest.of(page,4);
+        Iterable<Task> tasks = taskRepository.findAll(pageable);
         model.addAttribute("tasks", tasks);
         return "listTasks";
     }
@@ -48,6 +50,24 @@ public class GreetingController {
         return "redirect:/listTasks";
     }
 
+    @PostMapping("/listTasks/save")
+    public String save(Task t) {
+        taskRepository.save(t);
+        return "redirect:/listTasks";
+    }
+
+    @GetMapping("/listTasks/delete")
+    public String deleteTask(Task task) {
+        taskRepository.delete(task);
+        return "redirect:/listTasks";
+    }
+
+    @GetMapping("/listTasks/findOne")
+    @ResponseBody
+    public String findOne(int id) {
+        taskRepository.findById(id);
+        return "";
+    }
 }
 
 
--- src/main/java/com/expert1c/taskmgr/repository/TaskRepository.java
+++ src/main/java/com/expert1c/taskmgr/repository/TaskRepository.java
@@ -1,8 +1,9 @@
 package com.expert1c.taskmgr.repository;
 
 import com.expert1c.taskmgr.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TaskRepository extends CrudRepository<Task, Integer>{
+public interface TaskRepository extends JpaRepository<Task, Integer> {
 
 }
