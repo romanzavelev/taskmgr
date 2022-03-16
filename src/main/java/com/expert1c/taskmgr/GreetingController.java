c2653efaebfa1f0c1d542357750069869493eef6
Roman Zavelev <rzav@mail.ru>
2022-03-16 20:40:36 +0300

    1

--- src/main/java/com/expert1c/taskmgr/GreetingController.java
+++ src/main/java/com/expert1c/taskmgr/GreetingController.java
@@ -6,12 +6,13 @@ import com.expert1c.taskmgr.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 public class GreetingController {
@@ -65,15 +66,10 @@ public class GreetingController {
 
     @GetMapping("/listTasks/findOne")
     @ResponseBody
-    public String findOne(int id) {
-        taskRepository.findById(id);
-        return "";
+    public Optional<Task> findOne(int id) {
+        return taskRepository.findById(id);
     }
 
-    @GetMapping("/listTasks/modal")
-    public String testModal(Task task) {
-        return "modal";
-    }
 }
 
 
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,24 +1,35 @@
 $(document).ready(function(){
-    $('.table .eBtn').on('click',function(event){
+    $('.nBtn,.table .eBtn').on('click',function(event){
     event.preventDefault();
     var href = $(this).attr('href');
-    $.get(href,function(task){
-    $('.myForm #topic').val(task.topic);
-    $('.myForm #content').val(task.content);
-    $('.myForm #author').val(task.author);
-    })
+    var text = $(this).text();
+    if(text == 'Edit'){
+        $.getJSON(href, function (data) {
+            $('.myForm #id').val(data['id']);
+            $('.myForm #topic').val(data['topic']);
+            $('.myForm #content').val(data['content']);
+            $('.myForm #author').val(data['author']);
+        })
         $('.myForm #exampleModal').modal('show');
-       //console.log('Captain’s Log');
+        }else{
+         $('.myForm #id').val();
+                   $('.myForm #topic').val('');
+                    $('.myForm #content').val('');
+                    $('.myForm #author').val('');
+                     $('.myForm #exampleModal').modal('show');
+        }
     });
 
-    $(".table").dblclick(function(){
-        let one = $(this).children('td:first-child').text();
-        let two = $(this).children('td:nth-child(2)').text();
-        let three = $(this).children('td:last-child').text();
-        $('#one').val(one);
-        $('#two').val(two);
-        $('#three').val(three);
-        $("#idModalEditUser").modal('show');
-    });
+    // $(".table .eBtn").dblclick(function(event){
+    //     event.preventDefault();
+    //     var href = $(this).attr('href');
+    //     $.getJSON(href, function (data) {
+    //         $('.myForm #id').val(data['id']);
+    //         $('.myForm #topic').val(data['topic']);
+    //         $('.myForm #content').val(data['content']);
+    //         $('.myForm #author').val(data['author']);
+    //     });
+    //     $('.myForm #exampleModal').modal('show');
+    // });
 });
 
